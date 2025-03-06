96da03ebace500c5681fb76565b23626c06b3f16
Jeff Weston <21jeffweston@gmail.com>
2025-03-06 02:11:57 -0700

    feat: add color utility functions (2.2 Color Utility Functions)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
 int main() {
-    std::cout << "test" << std::endl;
-    return 0;
+    /*
+        - pixels written in top-to-btm rows
+        - rgb rep by 0.0 -> 1.0
+        - off to on:
+            red: left to right
+            green: top to btm 
+    */
+
+    // image
+    int image_width = 256;
+    int image_height = 256;
+    
+    // render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
